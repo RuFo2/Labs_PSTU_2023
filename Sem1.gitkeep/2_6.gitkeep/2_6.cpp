2b77d26a6d0a280b38c03558a97eaa61dfc989cf
RuFo2 <152308519+RuFo2@users.noreply.github.com>
2023-11-29 14:13:28 +0500

    Add files via upload

---
+++ Sem1.gitkeep/2_6.gitkeep/2_6.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	int stars = n;
+	int spaces = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < spaces; j++) {
+			cout << " ";
+		}
+		spaces+=2;
+		for (int j = 0; j < stars; j++) {
+			cout << " *";
+		}
+		stars--;
+		cout << endl;
+	}
+	return 0;
+}
