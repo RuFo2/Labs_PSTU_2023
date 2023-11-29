56aa64bd4aaa5549b004354b76ca9bae17307624
RuFo2 <152308519+RuFo2@users.noreply.github.com>
2023-11-29 14:31:03 +0500

    Add files via upload

---
+++ Sem1.gitkeep/2_7.gitkeep/2_7_1.gitkeep/2_7_1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 0; i < sqrt(n); i++) {
+		for (int j = 0; j < sqrt(n); j++) {
+			cout << " *";
+		}
+		cout << endl;
+	}
+	return 0;
+
+}
