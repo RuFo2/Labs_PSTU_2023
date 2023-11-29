10913aa002c91b64ef7f01c7c6ec3b0305e028b5
RuFo2 <152308519+RuFo2@users.noreply.github.com>
2023-11-29 13:19:11 +0500

    Add files via upload

---
+++ Sem1.gitkeep/2_3.gitkeep/2_3.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n, s=0, t=0;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        t = 1;
+        for (int j = i; j <= 2 * i; j++) {
+            t *= j;
+        }
+        s += t;
+    }
+    cout << s << endl;
+    return 0;
+}
