945468e26ad27a6732d3694f79f0a3733db812f3
RuFo2 <152308519+RuFo2@users.noreply.github.com>
2023-11-29 11:17:53 +0500

    Add files via upload

---
+++ Sem1.gitkeep/1_2.gitkeep/1_2.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	float a, b;
+	cin >> a >> b;
+	if (b!= 0) {
+		cout << a / b << endl;
+	}
+	else {
+		cout << "No result" << endl;
+	}
+}
