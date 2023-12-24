7614dd34d9cd42d9a7bcc44e68d4a28f03e1db9c
RuFo2 <152308519+RuFo2@users.noreply.github.com>
2023-12-24 13:39:29 +0500

    Add files via upload

---
+++ Sem1.gitkeep/pointers.gitkeep/4.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int n, tmp, max, min, *ptr=&tmp;
+	cin >> n;
+	cin >> *ptr;
+	max = *ptr;
+	min = *ptr;
+	for (int i = 2; i <= n; i++)
+	{
+		cin >> *ptr;
+		if (*ptr > max)
+		{
+			max = *ptr;
+		}
+		else if (*ptr < min)
+		{
+			min = *ptr;
+		}
+	}
+	cout << max <<  min << endl;
+	return 0;
+}
