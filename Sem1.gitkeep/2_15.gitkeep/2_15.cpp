776ea5109a08b29b17f68e709f22e5f8f084e55d
RuFo2 <152308519+RuFo2@users.noreply.github.com>
2023-12-24 07:20:40 +0500

    Add files via upload

---
+++ Sem1.gitkeep/2_15.gitkeep/2_15.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int n, s;
+	cin >> n >> s;
+	bool flag = false;
+	while (n > 0 && !flag)
+	{
+		if (n % 10 == s)
+		{
+			flag = true;
+			cout << "+" << endl;
+		}
+		else
+			n /= 10;
+	}
+	if (!flag)
+	{
+		cout << "-" << endl;
+	}
+	return 0;
+}
