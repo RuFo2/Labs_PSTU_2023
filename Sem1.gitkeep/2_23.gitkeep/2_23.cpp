4c8e95406d30770100300774fbb4941209452af5
RuFo2 <152308519+RuFo2@users.noreply.github.com>
2023-12-24 10:08:33 +0500

    Add files via upload

---
+++ Sem1.gitkeep/2_23.gitkeep/2_23.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    float n, max, a;
+    int i = 2;
+    int count = 1;
+    cin >> n;
+    max = round(sin(n + 1 / n) * 10) / 10;
+    while (i <= n)
+    {
+        a = sin(n + i / n);
+        a = round(a * 10) / 10;
+        if (a > max)
+        {
+            max = a;
+            count = 1;
+        }
+        else if (a == max)
+        {
+            count++;
+        }
+        i++;
+    }
+    cout << "max=" << max << " cnt=" << count << endl;
+    return 0;
+}
