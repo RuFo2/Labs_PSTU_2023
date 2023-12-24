dae027d017e3a72c47071f69e71bc318cb83db98
RuFo2 <152308519+RuFo2@users.noreply.github.com>
2023-12-24 14:42:51 +0500

    Add files via upload

---
+++ Sem1.gitkeep/pointers.gitkeep/6.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+int main(){
+    setlocale(LC_ALL, "ru_RU");
+    int countC = 0;
+    int countV = 0;
+    std::string st;
+    std::cout << "Введите строку " << std::endl;
+    std::cin >> st;
+    const char* pt = st.c_str();
+    for (int i = 0; i < st.length(); i++)
+    {
+        char tmp = *pt;
+        std::cout << *pt << std::endl;
+        if (tmp == L'a' || tmp == L'e' || tmp == L'y' || tmp == L'u' || tmp == L'i' || tmp == L'o')
+        {
+            countV++;
+        }
+        else { countC++; }
+        pt++;
+    }
+    std::cout << "Кол-во гласных: " << countV << std::endl;
+    std::cout << "Кол-во согласных: " << countC << std::endl;
+    return 0;
+}
