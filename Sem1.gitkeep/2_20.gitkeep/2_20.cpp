e2c407c056efc1dce9ec7c19bce4e14f2b6e4ab3
RuFo2 <152308519+RuFo2@users.noreply.github.com>
2023-12-24 09:06:35 +0500

    Add files via upload

---
+++ Sem1.gitkeep/2_20.gitkeep/2_20.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    float n, s, a;
+    int i = 1;
+    bool flag = false;
+    cin >> n >> s;
+    while (i <= n && !flag)
+    {
+        a = sin(n + i / n);
+        a = round(a * 10) / 10;
+        if (a == s)
+        {
+            flag = true;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    if (flag)
+    {
+        cout << "element is found" << endl;
+    }
+    else
+    {
+        cout << "element was not found" << endl;
+    }
+    return 0;
+}
