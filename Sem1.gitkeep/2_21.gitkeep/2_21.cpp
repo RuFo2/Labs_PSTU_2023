2785ac5a65bfece8787687bcb5ca8cf7661d6cc5
RuFo2 <152308519+RuFo2@users.noreply.github.com>
2023-12-24 09:37:38 +0500

    Add files via upload

---
+++ Sem1.gitkeep/2_21.gitkeep/2_21.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int n,tmp;
+	bool flag = true;
+	cin >> n;
+	while (n != 0)
+	{
+		cin >> tmp;
+		if (tmp != 0 && n > tmp)
+		{
+			flag = false;
+		}
+		n = tmp;
+	}
+	if (flag)
+	{
+		cout << "ordered" << endl;
+	}
+	else
+	{
+		cout << "unordered" << endl;
+	}
+	return 0;
+}
