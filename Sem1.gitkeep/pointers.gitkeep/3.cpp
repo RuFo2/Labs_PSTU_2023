9556cf9e58f68ab684a410490c5d0b348b08e082
RuFo2 <152308519+RuFo2@users.noreply.github.com>
2023-12-24 13:21:04 +0500

    Add files via upload

---
+++ Sem1.gitkeep/pointers.gitkeep/3.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	float a, b;
+	float* ptr1, * ptr2;
+	cin >> a >> b;
+	ptr1 = &a;
+	ptr2 = &b;
+	cout << (*ptr1 + *ptr2) << endl;
+	return 0;
+}
