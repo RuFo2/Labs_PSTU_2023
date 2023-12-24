96419ccb5579749b0d992ef544dc35fd18cf02c2
RuFo2 <152308519+RuFo2@users.noreply.github.com>
2023-12-24 05:09:23 +0500

    Add files via upload

---
+++ Sem1.gitkeep/2_10.gitkeep/2_10.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int n, max, tmp;
+	cin >> n;
+	cin >> max;
+	for (int i = 2; i <= n; i++)
+	{
+		cin >> tmp;
+		if (tmp > max)
+		{
+			max = tmp;
+		}
+	}
+	cout << max << endl;
+	return 0;
+}
