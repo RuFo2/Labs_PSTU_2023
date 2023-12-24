e905c37c50cd4e570306320de953c32f2673a28c
RuFo2 <152308519+RuFo2@users.noreply.github.com>
2023-12-24 10:02:29 +0500

    Add files via upload

---
+++ Sem1.gitkeep/2_22.gitkeep/2_22.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main(){
+    float n, max, a;
+    int i = 2;
+    int num = 1;
+    cin >> n;
+    max = sin(n + 1 / n);
+    while (i <= n)
+    {
+        a = sin(n + i / n);
+        if (a > max)
+        {
+            max = a;
+            num = i;
+        }
+        i++;
+    }
+    cout << "max:" << max << ", num:" << num << endl;
+    return 0;
+}
