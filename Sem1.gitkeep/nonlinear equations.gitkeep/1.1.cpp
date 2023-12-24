264ab3940ec21dbd7cbb1893a5febd29ed4bbd6d
RuFo2 <152308519+RuFo2@users.noreply.github.com>
2023-12-24 17:44:49 +0500

    Add files via upload

--- Sem1.gitkeep/nonlinear equations.gitkeep/1.1.cpp
+++ Sem1.gitkeep/nonlinear equations.gitkeep/1.1.cpp
@@ -1,1 +1,27 @@
+﻿// f(x) = 3*x - 4*ln(x) - 5
+// [2; 4]
 
+//Метод половинного деления
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    double a, b, c;
+    double fA, fB, fC;
+    double eps = 0.000001;
+    a = 2;
+    b = 4;
+    while (b - a > eps)
+    {
+        c = (a + b) / 2;
+        fA = (3 * a - 4 * log(a) - 5);
+        fB = (3 * b - 4 * log(b) - 5);
+        fC = (3 * c - 4 * log(c) - 5);
+        if (fA * fC < 0) { b = c; }
+        else if (fC * fB < 0) { a = c; }
+        else { cout << "no result" << endl; }
+    }
+    cout << "Result: " << a << "; " << b << endl;
+    return 0;
+}
