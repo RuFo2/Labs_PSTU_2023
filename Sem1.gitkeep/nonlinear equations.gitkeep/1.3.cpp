51bc5bb93eab3f11a7e6d76bcb2dd68f27fc5900
RuFo2 <152308519+RuFo2@users.noreply.github.com>
2023-12-24 17:45:33 +0500

    Add files via upload

---
+++ Sem1.gitkeep/nonlinear equations.gitkeep/1.3.cpp
@@ -0,0 +1,33 @@
+﻿// f(x) = 3*x - 4*log(x) - 5;
+// f'(x) = 3 - 4/x;
+// f''(x) = 4/x^2;
+// [2, 4]
+// 
+
+//ф(x) = x + λ(3 * x - 4 * log(x) - 5)
+// -1/r < λ < 0, если f'(x) > 0
+// 0 < λ < 1/r, если f'(x) < 0
+// r = max(|f'(a)|, |f'(b)|)
+// r = max(|3*2 - 4*log(2) - 5|, |3*4 - 4*log(4) - 5|) = max(1.773, 1.455) = 11
+// f'(x) > 0 => -1/r < λ < 0 => ~(-2/3;0)
+// берем любое удобное число в диапазоне
+// λ = -1/5 = -0.2
+
+//Метод итераций
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	float x, xPrev;
+	double lambda = -0.2;
+	x = 2;
+	xPrev = 0;
+	double eps = 0.000001;
+	while (abs(x - xPrev) > eps)
+	{
+		xPrev = x;
+		x = lambda * (3 * xPrev - 4 * log(xPrev) - 5) + xPrev;
+	}
+	cout << "Result " << x << endl;
+	return 0;
+}
