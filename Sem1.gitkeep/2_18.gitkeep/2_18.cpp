53928d06db2bfbb3612dc8fdde4cc4d120481d46
RuFo2 <152308519+RuFo2@users.noreply.github.com>
2023-12-24 08:11:45 +0500

    Add files via upload

---
+++ Sem1.gitkeep/2_18.gitkeep/2_18.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main(){
+	int n;
+	int re = 0;
+	cin >> n;
+	while (n > 0)
+	{
+		re = re * 10 + n % 10;
+		n /= 10;
+	}
+	cout << re << endl;
+	return 0;
+}
