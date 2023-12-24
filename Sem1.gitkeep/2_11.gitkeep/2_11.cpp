dde8a8ac797b441fd787b38791d0afda31735700
RuFo2 <152308519+RuFo2@users.noreply.github.com>
2023-12-24 05:47:10 +0500

    Add files via upload

---
+++ Sem1.gitkeep/2_11.gitkeep/2_11.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n, tmp;
+	bool plfirst = false;
+	bool minfirst = false;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> tmp;
+		if (!plfirst && !minfirst)
+		{
+			if (tmp > 0)
+			{ 
+				plfirst = true;
+			}
+			else if (tmp < 0)
+			{
+				minfirst = true;
+			}
+		}
+	}
+	if (plfirst)
+	{
+		cout << "+" << endl;
+	}
+	else if (minfirst)
+	{
+		cout << "-" << endl;
+	}
+	else
+	{
+		cout << 0 << endl;
+	}
+	return 0;
+}
