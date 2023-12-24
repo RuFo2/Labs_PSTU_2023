ab76b7940f13ab7b6512f9e42cd794c34794c921
RuFo2 <152308519+RuFo2@users.noreply.github.com>
2023-12-24 17:45:12 +0500

    Add files via upload

---
+++ Sem1.gitkeep/nonlinear equations.gitkeep/1.2.cpp
@@ -0,0 +1,24 @@
+﻿// f(x) = 3*x - 4*log(x) - 5;
+// f'(x) = 3 - 4/x;
+// f''(x) = 4/x^2;
+// [2, 4]
+
+//Метод итераций
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    float x, xPrev;
+    // f(b) * f''(b) > 0
+    // (12 - 1.38629 - 5)*(4/16) > 0 => x=b;
+    x = 4, xPrev = 0;
+    double eps = 0.000001;
+    while (abs(x - xPrev) > eps)
+    {
+        xPrev = x;
+        x = xPrev - (3 * xPrev - 4 * log(xPrev) - 5) / (3 - 4 / x);
+    }
+    cout << "Result "<< x << endl;
+    return 0;
+}
