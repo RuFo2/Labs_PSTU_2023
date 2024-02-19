a667de1813aa73d4425bfd9bf118aac5deaab8c2
RuFo2 <152308519+RuFo2@users.noreply.github.com>
2024-02-20 00:54:27 +0500

    Add files via upload

---
+++ Sem_2.gitkeep/Labs.gitkeep/Lab_5.gitkeep/Lab_5.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <ctime>
+int n = 5;
+using namespace std;
+
+void create(int arr[5][5]) {
+    srand((unsigned int)time(0));
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i][j] = (rand() % (100 - 20 + 1)) + 20;
+        }
+    }
+}
+
+void printArr(int arr[5][5]) {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << arr[i][j] << ' ';
+        }
+        cout << endl;
+    }
+}
+int check(int arr[5][5])
+{
+    int max = arr[0][0];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (arr[i][j] > max)
+            {
+                max = arr[i][j];
+            }
+        }
+    }
+    return max;
+}
+int main() {
+
+    int array[5][5];
+    create(array);
+    printArr(array);
+    cout << check(array) << endl;
+    return 0;
+}
