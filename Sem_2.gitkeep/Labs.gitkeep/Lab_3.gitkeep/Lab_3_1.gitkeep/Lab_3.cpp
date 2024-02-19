c24c9af66fd3e2c2db033676e4b29dc5847cc0ab
RuFo2 <152308519+RuFo2@users.noreply.github.com>
2024-02-20 00:33:27 +0500

    Add files via upload

---
+++ Sem_2.gitkeep/Labs.gitkeep/Lab_3.gitkeep/Lab_3_1.gitkeep/Lab_3.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+double sum(double n, double x)
+{
+    if (n == 0) return x;
+    else return  (pow(- 1, n) * (pow(x, (2 * n + 1)) / (2 * n + 1))) + sum(n - 1, x);
+}
+
+int main()
+{
+    double n, x, k = 0; 
+    cin >> n >> x;
+    cout << sum(n, x) << endl;
+    return 0;
+}
