db3f95ba07a8d6d4e7b018fec85645614b59f0a0
RuFo2 <152308519+RuFo2@users.noreply.github.com>
2024-02-20 00:25:41 +0500

    Add files via upload

---
+++ Sem_2.gitkeep/Labs.gitkeep/Lab_2.gikeep/Lab_2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num[8] = { 1,2,3,4,5,6,7,8 };
+	int arr[8][8];
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
+	for (int i = 0, k = 0; i < 8; k++, i++)
+	{
+		for (int j = 0; j < 8 - k; j++)
+		{
+			arr[i + j][j] = num[k % 8];
+		}
+	}
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			cout << arr[i][j] << ' ';
+		}
+		cout << endl;
+	}
+	return 0;
+}
