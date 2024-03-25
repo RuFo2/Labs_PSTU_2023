13264cfe2dd359938023be2794281c77cc2bdb2e
RuFo2 <152308519+RuFo2@users.noreply.github.com>
2024-03-25 16:02:17 +0500

    Add files via upload

---
+++ Sem_2.gitkeep/3_4.gitkeep/3.4.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	const int n = 5;
+	bool flag = true;
+	int a[n] = { 1,2,3,4,5 };
+	for (int i = 0; i < n - 1; i++)
+	{
+		if (a[i] > a[i + 1]) { flag = false; }
+	}
+	if (flag)
+	{
+		cout << "+" << endl;
+	}
+	else
+	{
+		cout << "-" << endl;
+	}
+	return 0;
+}
