fccec43deff289eec61dd99bc4daf06c7e293b08
RuFo2 <152308519+RuFo2@users.noreply.github.com>
2024-03-25 16:02:52 +0500

    Add files via upload

---
+++ Sem_2.gitkeep/3_5.gitkeep/3.5.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    const int n = 5;
+    int max;
+    int a[n] = { 1,2,3,4,5 };
+    max = a[0];
+    for (int i = 1; i < n; i++) {
+        if (max < a[i]) {
+            max = a[i];
+        }
+    }
+    cout << max << endl;
+    return 0;
+}
