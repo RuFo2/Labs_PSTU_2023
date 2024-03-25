ceb58d8a88123f5820072f45276029b1fa38efbe
RuFo2 <152308519+RuFo2@users.noreply.github.com>
2024-03-25 16:01:09 +0500

    Add files via upload

---
+++ Sem_2.gitkeep/3_1.gitkeep/3.1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	const int n = 4;
+	int tmp;
+	int arr[n] = { 41,42,66,77 };//если arr[n] не работает, то забиваем целочисленный литерал. кол-во элементов здесь несильно важно
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	for (int i = 0; i < n / 2; i++)
+	{
+		tmp = arr[i];
+		arr[i] = arr[n - i - 1];
+		arr[n - i - 1] = tmp;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	return 0;
+}
