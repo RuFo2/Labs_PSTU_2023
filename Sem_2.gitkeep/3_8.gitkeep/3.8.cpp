37051d9451ff32e8787a9d29f006df4544645506
RuFo2 <152308519+RuFo2@users.noreply.github.com>
2024-03-25 16:05:20 +0500

    Add files via upload

---
+++ Sem_2.gitkeep/3_8.gitkeep/3.8.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    int a[100];
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        a[i] = rand() % 100;
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] % 2 == 0)
+        {
+            for (int j = n - 1; j > i - 1; j--)
+            {
+                a[j + 1] = a[j];
+            }
+            a[i] = -1;
+            i++;
+            n++;
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
