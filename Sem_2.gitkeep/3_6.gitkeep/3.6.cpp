f6ef8474cb93f49cb563b0eabca83c21ce69bbb1
RuFo2 <152308519+RuFo2@users.noreply.github.com>
2024-03-25 16:03:51 +0500

    Add files via upload

---
+++ Sem_2.gitkeep/3_6.gitkeep/3.6.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int n = 8;
+    int k=0;
+    int a[n] = { 5,5,2,5,4,3,4,5 };
+    int m = a[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i] > m)
+        {
+            m = a[i];
+            k = 0;
+        }
+        if (a[i] == m)
+        {
+            k++;
+        }
+    }
+    if (a[0] == m)
+    {
+        k++;
+    }
+    cout << m << "  " << k;
+    return 0;
+}
