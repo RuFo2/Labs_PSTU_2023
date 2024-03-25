981d691d11b2f17fbfdd3761fa23f21cbf7436e1
RuFo2 <152308519+RuFo2@users.noreply.github.com>
2024-03-25 16:01:29 +0500

    Add files via upload

---
+++ Sem_2.gitkeep/3_2.gitkeep/3.2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    const int n = 11;
+    int tmp, p, q;
+    p = 2;
+    q = 6;
+    int arr[n] = { 41, 42, 66, 87, 86, 38, 77, 84, 45, 35, 22 };
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    while (p < q)
+    {
+        tmp = arr[p];
+        arr[p] = arr[q];
+        arr[q] = tmp;
+        p++;
+        q--;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
