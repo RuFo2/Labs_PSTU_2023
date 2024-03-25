392cd08f9b9e835516d5f35f664fe7ff1924506a
RuFo2 <152308519+RuFo2@users.noreply.github.com>
2024-03-25 16:04:41 +0500

    Add files via upload

---
+++ Sem_2.gitkeep/3_7.gitkeep/3.7.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    const int n = 5;
+    int m, p;
+    int a[n] = { 1,2,3,4,5 };
+    p = a[0];
+    m = a[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (p < a[i])
+        {
+            p = a[i];
+        }
+        else if (m > a[i])
+        {
+            m = a[i];
+        }
+    }
+    cout << p << "  " << m;
+    return 0;
+}
