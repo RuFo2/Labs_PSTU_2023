5c628e6288976ed4a4ca724bde4776090e9bc5f3
RuFo2 <152308519+RuFo2@users.noreply.github.com>
2024-03-26 13:46:27 +0500

    Add files via upload

---
+++ Sem_2.gitkeep/Labs.gitkeep/Lab_19.gitkeep/Lab_19.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+class arProg {
+    double first;
+    unsigned int second;
+
+public:
+    void set(double a, unsigned int b)
+    {
+        first = a;
+        second = b;
+    }
+    void element(int j)
+    {
+        cout << "a(j) = " << first * pow(second, j);
+    }
+};
+int main()
+{
+    arProg el;
+    el.set(1, 3);
+    el.element(5);
+    return 0;
+}
