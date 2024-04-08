371f3b29b3b3dfaaa49f966a0b5eb9ac0676acda
RuFo2 <152308519+RuFo2@users.noreply.github.com>
2024-04-08 14:10:37 +0500

    Add files via upload

---
+++ Sem_2.gitkeep/Labs.gitkeep/Lab_13.gitkeep/Lab_13.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+void shellSort(int arr[], int n)
+{
+    for (int h = n / 2; h > 0; h /= 2)
+    {
+        for (int i = h; i < n; i++)
+        {
+            int tmp = arr[i];
+            int j = 0;
+            for (j = i; j >= h && arr[j - h] > tmp; j -= h)
+            {
+                arr[j] = arr[j - h];
+            }
+            arr[j] = tmp;
+        }
+    }
+}
+
+void showArr(int* arr, int size) {
+    for (int i = 0; i < 6; i++)
+    {
+        cout << arr[i] << ' ';
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[6] = { 7, 8, 9, 5, 4, 2 };
+    showArr(arr, 6);
+    cout << endl;
+    shellSort(arr, 6);
+    showArr(arr, 6);
+
+    return 0;
+}
